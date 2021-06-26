0a7e6215ed9c57bd834455dc138ee9eccc1a1fc3
0xlilith <0xlilith@protonmail.com>
2021-06-27 03:40:31 +0530

    added 3 features: test, cleartext, bot-status

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-//const config = require("./config.json")
 
-client.on('ready', () => {
+const config = require("./config.json");
+const command = require("./command");
+
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
-});
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
-});
+  // test command
+  command(client, "test", (message) => {
+    message.channel.send("testing..");
+  });
 
-client.login(process.env.TOKEN);
+  // counts total members
+  command(client, "servers", (message) => {
+    client.guilds.cache.forEach((guild) => {
+      message.channel.send(
+		  `Name: ${guild.name} Members: ${guild.memberCount}`
+		);
+    });
+  });
+
+  command(client, ['cc', 'clearchannel'], message => {
+	  if (message.member.hasPermission('ADMINISTRATOR')) {
+		  message.channel.messages.fetch().then((results) => {
+			  message.channel.bulkDelete(results)
+		  })
+	  }
+  })
+
+  // to update the bot status
+  command(client, 'status', message => {
+	  const content = message.content.replace('!status', '')
+
+	  client.user.setPresence({
+		activity: {
+			name: content,
+			type: 0,
+		},
+	  })
+  })
+})
+
+//client.login(process.env.TOKEN);
+client.login(config.token);
