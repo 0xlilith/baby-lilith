ea52aa60e55b522a2b036cab73ce8e7b05196e72
0xlilith <0xlilith@protonmail.com>
2021-06-27 01:31:01 +0530

    it runs

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const Discord = require("discord.js")
-const client = new Discord.Client()
+const Discord = require('discord.js');
+const client = new Discord.Client();
 const config = require("./config.json")
 
 client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interaction', async interaction => {
-	if (!interaction.isCommand()) return;
-	if (interaction.commandName === 'ping') {
-		await interaction.reply('Pong!');
-	}
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
 });
 
 client.login(config.token);
