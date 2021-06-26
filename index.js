668901291092f899a4cf54130bda3803432099b6
0xlilith <>
2021-06-26 18:54:20 +0000

    using env instead of json

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const Discord = require("discord.js")
+const client = new Discord.Client()
+//const config = require("./config.json")
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interaction', async interaction => {
+	if (!interaction.isCommand()) return;
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
+
+client.login(config.token);
