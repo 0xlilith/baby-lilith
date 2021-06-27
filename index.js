0910838f8ed63c2500cba7fed58152830f2398e0
0xlilith <0xlilith@protonmail.com>
2021-06-27 18:14:42 +0530

    many features added

--- index.js
+++ index.js
@@ -3,42 +3,79 @@ const client = new Discord.Client();
 
 const config = require("./config.json");
 const command = require("./command");
+//const fMessage = require("./fMessage")
+const pmessage = require("./pmessage")
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
   // test command
   command(client, "test", (message) => {
-    message.channel.send("testing..");
+    message.channel.send("testing...");
   });
 
+  // info command
+  command()
+
   // counts total members
-  command(client, "servers", (message) => {
+  command(client, "server", (message) => {
     client.guilds.cache.forEach((guild) => {
       message.channel.send(
-		  `Name: ${guild.name} Members: ${guild.memberCount}`
-		);
+        `Name: ${guild.name} Members: ${guild.memberCount}`
+      );
     });
   });
 
-  command(client, ['cc', 'clearchannel'], message => {
-	  if (message.member.hasPermission('ADMINISTRATOR')) {
-		  message.channel.messages.fetch().then((results) => {
-			  message.channel.bulkDelete(results)
-		  })
-	  }
+  // clear chat
+  command(client, ['cc', 'clearchat'], message => {
+    if (message.member.hasPermission('ADMINISTRATOR')) {
+      message.channel.messages.fetch().then((results) => {
+        message.channel.bulkDelete(results)
+      })
+    }
   })
 
   // to update the bot status
   command(client, 'status', message => {
-	  const content = message.content.replace('!status', '')
+    const content = message.content.replace('+status', '')
 
-	  client.user.setPresence({
-		activity: {
-			name: content,
-			type: 0,
-		},
-	  })
+    client.user.setPresence({
+      activity: {
+        name: content,
+        type: 0,
+      },
+    })
+  })
+
+  // reaction message
+  //fMessage(client, '858399914803200020', "test test!!", ['🇯🇵','👽'])
+
+  // private message to user
+  pmessage(client, "baby-lilith pm", "hello")
+
+  // create text channel
+  command(client, 'crtch', (message) => {
+    const name = message.content.replace('+crtch ', '')
+    //const user = message.
+
+    message.guild.channels.create(name, {
+      type: 'text'
+    }).then(channel => {
+      //console.log(channel)
+      message.channel.send(`<@${message.author.id}> \`Created Text Channel:\` <#${channel.id}>`)
+    })
+  })
+
+  // create voice channel
+  command(client, 'crtvc', (message) => {
+    const name = message.content.replace('+crtvc ', '')
+    const user = message.author
+
+    message.guild.channels.create(name, {
+      type: 'voice'
+    }).then(channel => {
+      message.channel.send(`<@${message.author.id}> \`Created Voice Channel:\` <#${channel.id}>`)
+    })
   })
 })
 
